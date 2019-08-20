eaf3d3a4a5ba91c9bad05d11ca7a7b7b86a1af7f
Sujoy Ghosal <sujoy.ghosal@gmail.com>
2019-08-20 16:40:27 +0530

    Wave3 UAT

--- batch.sh
+++ batch.sh
@@ -29,7 +29,7 @@ generateNewSwagger.sh CorporateTreasury 12
 generateNewSwagger.sh PartyAuthentication 19
 generateNewSwagger.sh AdvancedVoiceServicesOperations 9
 generateNewSwagger.sh CardAuthorization 14
-generateNewSwagger.sh  CardCase 17
+generateNewSwagger.sh CardCase 17
 generateNewSwagger.sh CardCollections 15
 generateNewSwagger.sh CardFinancialSettlement 11
 generateNewSwagger.sh CardNetworkParticipantFacility 11
@@ -43,49 +43,68 @@ generateNewSwagger.sh ContactHandler 16
 generateNewSwagger.sh CreditChargeCard 27
 generateNewSwagger.sh ContactRouting 11
 generateNewSwagger.sh CreditManagement 5
-
+generateNewSwagger.sh CustomerAgreement 19
+generateNewSwagger.sh CurrentAccount 29
+generateNewSwagger.sh CustomerBehavioralInsights 9
+generateNewSwagger.sh CustomerBehaviorModels 15
+generateNewSwagger.sh CustomerCase 14
+generateNewSwagger.sh CustomerBilling 15
+generateNewSwagger.sh CustomerEventHistory 22
+generateNewSwagger.sh CustomerCreditRating 12
+generateNewSwagger.sh CustomerOffer 34
+generateNewSwagger.sh CustomerPosition 15
+generateNewSwagger.sh CustomerReferenceDataManagement 22
+generateNewSwagger.sh CustomerProductServiceEligibility 10
 
 Done So far
-ACHFulfillment.yaml:    ## Executive Summary:
-AccountRecovery.yaml:    ## Executive Summary:
-AdvancedVoiceServicesOperations.yaml:    ## Executive Summary:
-CardAuthorization.yaml:    ## Executive Summary:
-CardCase.yaml:    ## Executive Summary:
-CardClearing.yaml:    ## Executive Summary:
-CardCollections.yaml:    ## Executive Summary:
-CardECommerceGateway.yaml:    ## Executive Summary:
-CardFinancialSettlement.yaml:    ## Executive Summary:
-CardNetworkParticipantFacility.yaml:    ## Executive Summary:
-CardTerminalAdministration.yaml:    ## Executive Summary:
-CardTerminalOperation.yaml:    ## Executive Summary:
-CardTransactionSwitch.yaml:    ## Executive Summary:
-ChannelActivityAnalysis.yaml:    ## Executive Summary:
-ChannelActivityHistory.yaml:    ## Executive Summary:
-CollateralAllocationManagement.yaml:    ## Executive Summary:
-CollateralAssetAdministration.yaml:    ## Executive Summary:
-ConsumerLoan.yaml:    ## Executive Summary:
-ContactDialogue.yaml:    ## Executive Summary:
-ContactHandler.yaml:    ## Executive Summary:
-ContactRouting.yaml:    ## Executive Summary:
-CorporateTreasury.yaml:    ## Executive Summary:
-Correspondence.yaml:    ## Executive Summary:
-CorrespondentBank.yaml:    ## Executive Summary:
-CorrespondentBankDataManagement.yaml:    ## Executive Summary:
-CreditChargeCard.yaml:    ## Executive Summary:
-CustomerAccessEntitlement.yaml:    ## Executive Summary:
-CustomerProfile.yaml:    ## Executive Summary:
-Disbursement.yaml:    ## Executive Summary:
-DocumentServices.yaml:    ## Executive Summary:
-EBranchOperations.yaml:    ## Executive Summary:
-FinancialGateway.yaml:    ## Executive Summary:
-PartyAuthentication.yaml:    ## Executive Summary:
-PartyLifeCycleManagement.yaml:    ## Executive Summary:
-PaymentExecution.yaml:    ## Executive Summary:
-PaymentInitiation.yaml:    ## Executive Summary:
-PaymentOrder.yaml:    ## Executive Summary:
-PointOfService.yaml:    ## Executive Summary:
-PositionKeeping.yaml:    ## Executive Summary:
-ProductDirectory.yaml:    ## Executive Summary:
-SavingsAccount.yaml:    ## Executive Summary:
-ServicingEventHistory.yaml:    ## Executive Summary:
-TransactionAuthorization.yaml:    ## Executive Summary:
+ACHFulfillment.yaml
+AccountRecovery.yaml
+AdvancedVoiceServicesOperations.yaml
+CardAuthorization.yaml
+CardCase.yaml
+CardClearing.yaml
+CardCollections.yaml
+CardECommerceGateway.yaml
+CardFinancialSettlement.yaml
+CardNetworkParticipantFacility.yaml
+CardTerminalAdministration.yaml
+CardTerminalOperation.yaml
+CardTransactionSwitch.yaml
+ChannelActivityAnalysis.yaml
+ChannelActivityHistory.yaml
+CollateralAllocationManagement.yaml
+CollateralAssetAdministration.yaml
+ConsumerLoan.yaml
+ContactDialogue.yaml
+ContactHandler.yaml
+ContactRouting.yaml
+CorporateTreasury.yaml
+Correspondence.yaml
+CorrespondentBank.yaml
+CorrespondentBankDataManagement.yaml
+CreditChargeCard.yaml
+CreditManagement.yaml
+CurrentAccount.yaml
+CustomerAccessEntitlement.yaml
+CustomerAgreement.yaml
+CustomerBehaviorModels.yaml
+CustomerBehavioralInsights.yaml
+CustomerBilling.yaml
+CustomerCase.yaml
+CustomerCreditRating.yaml
+CustomerProfile.yaml
+Disbursement.yaml
+DocumentServices.yaml
+EBranchOperations.yaml
+FinancialGateway.yaml
+PartyAuthentication.yaml
+PartyLifeCycleManagement.yaml
+PaymentExecution.yaml
+PaymentInitiation.yaml
+PaymentOrder.yaml
+PointOfService.yaml
+PositionKeeping.yaml
+ProductDirectory.yaml
+SavingsAccount.yaml
+ServicingEventHistory.yaml
+TransactionAuthorization.yaml
--- genConfigs.sh
+++ genConfigs.sh
@@ -3,24 +3,61 @@ cat CR|sed -e $'s/\t/|/g' -e $'s/"//g'>p
 cat $1SpecificModel|sed -e $'s/\t/|/g' -e $'s/"//g'>s
 cp BQ.txt BQ
 i=""
-echo "Enter SD"
+#echo "Enter SD"
 read sd
-echo "Enter CR"
+#echo "Enter CR"
 read cr
-echo "Enter a CR Verb"
+read allBQs
+echo "Processing SD $sd, CR $cr, BQs $allBQs ..."
+read metadesc
+read metaeg
+read metaexecsum
+read summaries
+metainfo="	$metaexecsum	$metaeg	$metadesc"
+echo "Reading CR Verbs..."
 firstLine="CR $cr Instance Record|$cr||||||"
 output="crbqmodel.txt"
 pathConfig="crbqpath.txt"
 rm -rf b out o bqfile o.txt $output $pathConfig
-echo "sd=$sd" >$pathConfig
+echo "sd=$sd$metainfo" >$pathConfig
 echo "sdpath=`echo $sd|sed 's/[ (/-]/-/g'| tr '[:upper:]' '[:lower:]'`" >>$pathConfig
 echo "crpath=`echo $cr|sed 's/[ (/-]/-/g'| tr '[:upper:]' '[:lower:]'`">>$pathConfig
 echo "crr=$cr Instance">>$pathConfig
-echo "mcr=`echo $cr|sed 's/[ (/-]//g'`">>$pathConfig
-echo "bqs=REPLACE WITH BQS">>$pathConfig
+echo "mcr=`echo $cr|sed 's/[(/-]//g'`">>$pathConfig
+#echo "bqs=REPLACE WITH BQS">>$pathConfig
+echo "bqs=$allBQs">>$pathConfig
 echo "CONFIG">>$pathConfig
 insertCR="true"
 insertBQ="true"
+j=0
+k=0
+m=""
+n[0]=""
+while read l
+do
+	k=`expr $k + 1`
+	if [ $k -gt  7 ]
+	then
+			if [ "$l" != "done" ]
+			then
+					if [ "$l" != "Retrieve" ]
+					then
+						m[$j]="|I"${m[$j]}
+					fi
+					#echo $l
+					n[$j]=${n[$j]}"|"
+			else
+					#echo "${n[j]}"
+					#echo "${m[j]}"
+					r=$j
+					j=`expr $j + 1`
+					n[$j]=${n[$r]}
+					read l
+			fi
+	fi
+done<Verbs
+summariesCount=0
+mCount=0
 while read verb
 do
 	if [[ "$verb" == "done" ]];
@@ -28,6 +65,7 @@ do
 		break;
 	fi
 	i="|"$i
+	summariesCount=`expr $summariesCount + 1`
 	verb="$(tr '[:lower:]' '[:upper:]' <<< ${verb:0:1})${verb:1}"
 	firstLine=$firstLine"CR $cr $verb""|"
 	verb=`echo $verb|sed 's/[ (/-]//g'| tr '[:upper:]' '[:lower:]'`
@@ -36,7 +74,26 @@ do
 			a=`echo $line|cut -f1 -d"|"|sed 's/[ (/-]//g'| tr '[:upper:]' '[:lower:]'`
 			if [[ "$a" == "mandatory" ]] && [[ "$insertCR" == "true" ]]
 			then
-				echo $line|cut -f1-7 -d"|"|sed "s/#CR/$cr/g"|sed "s/#SD/$sd/g">>out
+				if [ $mCount -eq 0 ]
+				then
+					echo `echo $line|cut -f1-7 -d"|"|sed "s/#CR/$cr/g"|sed "s/#SD/$sd/g"`"${m[0]}">>out
+					mCount=1
+				else
+					mCount=0
+					#echo "Original ${m[0]}"
+					firstMarker=${m[0]}
+					case $verb in
+						initiate|create|activate|register|evaluate|provide)
+						firstMarker="|O|"`echo ${m[0]}|cut -f3- -d"|"`
+						#echo "$verb second line CR markers: $firstMarker"
+						;;
+						*)
+						;;
+					esac
+					#firstMarker="|O|"`echo ${m[0]}|cut -f3- -d"|"`
+					#echo "Replaced $firstMarker"
+					echo `echo $line|cut -f1-7 -d"|"|sed "s/#CR/$cr/g"|sed "s/#SD/$sd/g"`$firstMarker>>out
+				fi
 				continue
 			fi
 			if [[ "$a" == "$verb" ]]
@@ -91,13 +148,16 @@ do
 			fi
 	done<p
 	echo "Processed $verb"
+	summary=`echo $summaries|cut -f$summariesCount -d"|"`
 	crp=`echo $cr|sed 's/[ (/-]//g'`
-	echo "$crp	$verb	#summary		$verb$crp		#desc" >>$pathConfig
+	echo "$crp	$verb	$summary		$verb$crp		#desc" >>$pathConfig
 	cat out>o.txt
 done
 cat BQ|sed -e $'s/\t/|/g' -e $'s/"//g'>bqfile
 bqs=
-echo "Enter BQ"
+j=0
+mCount=0
+echo "Reading BQ"
 while read bq
 do
 	if [[ "$bq" == "done" ]];
@@ -108,9 +168,12 @@ do
 	insertBQ="true"
 	insertMan="true"
 	bqnospace=`echo $bq|sed 's/[ (/-]//g'`
-	bqs="$bqs $bq"
+#	bqs="$bqs $bq"
+	k=$j
+	j=`expr $j + 1`
+	bqs="$bqs $bqnospace"
 	echo "BQ $bq Instance Record|$bq||||||">bqout
-	echo "Enter verb for $bq"
+	echo "Reading verbs for $bq"
 	while read verb
 	do
 		verb=`echo $verb|sed 's/[ (/-]//g'| tr '[:upper:]' '[:lower:]'`
@@ -119,6 +182,7 @@ do
 			break;
 		fi
 		i="|"$i
+		summariesCount=`expr $summariesCount + 1`
 		bqm=`echo $bq|sed 's/[ (/-]//g'`
 		crm=`echo $cr|sed 's/[ (/-]//g'`
 		vu="$(tr '[:lower:]' '[:upper:]' <<< ${verb:0:1})${verb:1}"
@@ -129,48 +193,69 @@ do
 			if [[ "$b" == "mandatory" ]] && [[ "$insertMan" == "true" ]]
 			then
 				#echo "Writing BQ Mandatory.."
-				echo $line|cut -f1-7 -d"|"|sed "s/#CR/$cr/g"|sed "s/#BQ/$bq/g">>bqout
+				if [ $mCount -eq 0 ]
+				then
+					ml=`echo $line|cut -f1-7 -d"|"|sed "s/#CR/$cr/g"|sed "s/#BQ/$bq/g"`"${n[$k]}""${m[$j]}"
+					echo $ml>>bqout
+					mCount=1
+				else
+					#echo "Original ${m[$j]}"
+					firstMarker=${m[$j]}
+					case $verb in
+						initiate|create|activate|register|evaluate|provide)
+							firstMarker="|O|"`echo ${m[$j]}|cut -f3- -d"|"`
+							#echo "$verb second line BQ markers: $firstMarker"
+						;;
+						*)
+						;;
+					esac
+					#echo "Replaced $firstMarker"
+					ml=`echo $line|cut -f1-7 -d"|"|sed "s/#CR/$cr/g"|sed "s/#BQ/$bq/g"`"${n[$k]}"$firstMarker
+					echo $ml>>bqout
+					mCount=0
+				fi
 				continue
 			fi
 			if [[ "$b" == "$verb" ]]
 			then
 				f7=`echo $line|cut -f1-7 -d"|"| sed "s/#BQ/$bq/g"`
+				summary=`echo $summaries|cut -f$summariesCount -d"|"`
 				case $verb in
 					initiate|create|activate|register|evaluate|provide)
 						fr=$i`echo $line|cut -f17 -d"|"`
-						operations="$bqm	$verb	#summary	$verb$crm$bqm	$verb$crm$bqm		Details of a new $bq instance"
+						operations="$bqm	$verb	$summary	$verb$crm$bqm	$verb$crm$bqm		Details of a new $bq instance"
 						;;
 					update)
 						fr=$i`echo $line|cut -f18 -d"|"`
-						operations="$bqm	$verb	#summary	$verb$crm$bqm	$verb$crm$bqm		Update to any amendable fields of the $bq instance"
+						operations="$bqm	$verb	$summary	$verb$crm$bqm	$verb$crm$bqm		Update to any amendable fields of the $bq instance"
 						;;
 					control) 
 						fr=$i`echo $line|cut -f19 -d"|"`
-						operations="$bqm	$verb	#summary	$verb$crm$bqm	$verb$crm$bqm		Request specific processing (e.g. suspend, skip, terminate)"
+						operations="$bqm	$verb	$summary	$verb$crm$bqm	$verb$crm$bqm		Request specific processing (e.g. suspend, skip, terminate)"
 						;;
 					exchange) 
 						fr=$i`echo $line|cut -f20 -d"|"`
-						operations="$bqm	$verb	#summary	$verb$crm$bqm	$verb$crm$bqm		Handle an external exchange (e.g. accept, reject, verify)"
+						operations="$bqm	$verb	$summary	$verb$crm$bqm	$verb$crm$bqm		Handle an external exchange (e.g. accept, reject, verify)"
 						;;
 					capture) 
 						fr=$i`echo $line|cut -f21 -d"|"`
-						operations="$bqm	$verb	#summary	$verb$crm$bqm	$verb$crm$bqm		Provide a structured input transaction/record (e.g. timesheet, event)"
+						operations="$bqm	$verb	$summary	$verb$crm$bqm	$verb$crm$bqm		Provide a structured input transaction/record (e.g. timesheet, event)"
 						;;
 					execute) 
 						fr=$i`echo $line|cut -f22 -d"|"`
-						operations="$bqm	$verb	#summary	$verb$crm$bqm	$verb$crm$bqm		Invoke an automated execute action against the $bq instance"
+						operations="$bqm	$verb	$summary	$verb$crm$bqm	$verb$crm$bqm		Invoke an automated execute action against the $bq instance"
 					;;
 					request) 
 						fr=$i`echo $line|cut -f23 -d"|"`
-						operations="$bqm	$verb	#summary	$verb$crm$bqm	$verb$crm$bqm		Invoke a service request action against the $bq instance"
+						operations="$bqm	$verb	$summary	$verb$crm$bqm	$verb$crm$bqm		Invoke a service request action against the $bq instance"
 					;;
 					grant) 
 						fr=$i`echo $line|cut -f24 -d"|"`
-						operations="$bqm	$verb	#summary	$verb$crm$bqm	$verb$crm$bqm		Invoke a grant request action from the $bq instance to obtain access permission"
+						operations="$bqm	$verb	$summary	$verb$crm$bqm	$verb$crm$bqm		Invoke a grant request action from the $bq instance to obtain access permission"
 					;;
 					retrieve) 
 						fr=$i`echo $line|cut -f25 -d"|"`
-						operations="$bqm	$verb	#summary	$verb$crm$bqm	$verb$crm$bqm		Invoke a reporting action to obtain a $bq instance related report"
+						operations="$bqm	$verb	$summary	$verb$crm$bqm	$verb$crm$bqm		Invoke a reporting action to obtain a $bq instance related report"
 					;;
 					*)
 					;;
@@ -209,7 +294,7 @@ do
 	done ## end read bq verb
 
 	cat bqout>>o.txt
-	echo "Enter another BQ..."
+	echo "Reading another BQ..."
 
 done ## end read BQ
 echo $firstLine>$output
